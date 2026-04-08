bb247bafcb46ea7ce882bbafd1769e66fc107397
Yongjun Tan <3520097134@qq.com>
2026-04-08 18:55:42 +0800

    init project

---
+++ src/main/java/hmdp/HmdpApplication.java
@@ -0,0 +1,13 @@
+package hmdp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HmdpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HmdpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hmdp/HmdpApplicationTests.java
@@ -0,0 +1,13 @@
+package hmdp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HmdpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
